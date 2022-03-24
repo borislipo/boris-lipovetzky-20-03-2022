6b14dec77735a76d428fd870e34d9a5d8ab70e37
borislipo <boris.lipo123@gmail.com>
2022-03-24 12:51:04 +0200

    changed api key

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-export const apiKey = 'gqf4L3YmzXYc6YfWhQGs86L2Ifu6Rk45';
+export const apiKey = '7nV7r4xjM4LkixlC1ngt16WU1ZsDPr8g';
 export const url = 'https://dataservice.accuweather.com';
 export const telAvivKey = '215854';
 export const telAvivLabel = 'Tel Aviv';
