e26e81d04175c5eb50e27b7bcd7c833a36106d0a
borislipo <boris.lipo123@gmail.com>
2022-03-24 21:11:35 +0200

    deleted unused file

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-export const apiKey = 'GBsAArfaLJGh4eP7EYajC7559axSCVW2';
+export const apiKey = 'jmARuVECzM0oH9atn31wAASw9Bu1pOHL';
 export const url = 'https://dataservice.accuweather.com';
 export const telAvivKey = '215854';
 export const telAvivLabel = 'Tel Aviv';
--- src/weatherApp.js
+++ src/weatherApp.js
@@ -1,7 +1,6 @@
 import { Provider } from 'react-redux';
 import { AppRouter } from './routers/appRouter';
 import { store } from './store/store';
-import './App.css';
 
 export const WeatherApp = () => {
   return (
